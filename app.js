c66aa6e278f223a1522dd3266e9e490d134e8339
Lynux Ansay <lynux_ansay@dlsu.edu.ph>
2019-10-14 17:58:54 +0800

    node init

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path')
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+// Body Parser
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handlebars
+const exphbs  = require('express-handlebars');
+app.engine( 'hbs', exphbs( {
+    extname: '.hbs',
+    defaultLayout: 'main',
+    helpers: {
+      json: function (content) { return JSON.stringify(content); },
+      ifEquals: function(arg1, arg2, options) {return (arg1 == arg2) ? options.fn(this) : options.inverse(this);}
+    },
+    layoutsDir: path.join(__dirname, "views/layouts/"),
+    partialsDir: path.join(__dirname, 'views/partials')
+  }));
+
+app.set('view engine', '.hbs');
+
+
+// Routes
+app.use(express.static(__dirname + '/public')); //Public
+
+app.get('/', (req,res) =>{
+    res.render('login')
+})
+
+  
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
