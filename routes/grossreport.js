dbb6ae2a138a7ff93cf32b628f533b0a989cfa9c
Kyle T <kyle_tagle@dlsu.edu.ph>
2019-11-30 21:51:52 +0800

    navigation fixes

--- routes/grossreport.js
+++ routes/grossreport.js
@@ -12,5 +12,23 @@ router.get('/', (req, res) => {
     res.render('login')
 })
 
+router.post('/passwordmanager', (req, res) => {
+    let password = req.body.pw
+    let type = 'CO'
+  
+    User.findOne({type:type}, function(err, doc) {
+      if(err) {
+        console.log(err)
+      }
+      if(doc && password == doc.password) {
+        console.log(doc.password)
+        console.log(password)
+        res.render("passwordmanager.hbs")
+      }
+      else{
+        res.redirect('dashboard')
+      }
+    })
+  })
 
 module.exports = router;
