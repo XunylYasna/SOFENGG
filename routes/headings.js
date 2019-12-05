32fe016b6fbf14c096bf70aabd884256c377b7e8
Lynux Ansay <lynux_ansay@dlsu.edu.ph>
2019-12-06 04:46:31 +0800

    export

--- routes/headings.js
+++ routes/headings.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
 
     res.render("headings.hbs",
         {
+            layout: 'dashboardLayout',
             heading,
             poNumber,
             prfNumber
